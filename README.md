c4e1b4220f900ec1707c0f87b3b9deed03c2649d
Vu Tran <vu@vu-tran.com>
2016-09-04 01:09:52 -0700

    Initial Commit :sparkles:

---
+++ README.md
@@ -0,0 +1,26 @@
+# dext-docker-registry-plugin
+
+> [Dext](https://github.com/vutran/dext) plugin to search the Docker Registry for Docker images.
+
+![](screenshot.png?raw=true)
+
+## Install
+
+```bash
+$ dpm install dext-docker-registry-plugin
+```
+
+*Requires [Dext](https://github.com/vutran/dext).*
+
+## Usage
+
+In Dext, type `docker`, then your query.
+
+## Related
+
+- [dext](https://github.com/vutran/dext) - The Dext smart launcher
+- [dext-default-theme](https://github.com/vutran/dext-default-theme) - The default theme for Dext
+
+## License
+
+MIT © [Vu Tran](https://github.com/vutran/)
---
+++ index.js
@@ -0,0 +1,52 @@
+const got = require('got');
+
+const VERSION = 'v2';
+const ENDPOINT = `https://hub.docker.com/${VERSION}/search/repositories/`;
+
+/**
+ * Retrieve the repository URL for the given item
+ *
+ * If the result is an official repo, append the "_" prefix path.
+ *
+ * @param {Object} result
+ * @return {String}
+ */
+const getRepositoryUrl = result => {
+  const repo = result.is_official ? `_/${result.repo_name}` : result.repo_name;
+  return `https://hub.docker.com/r/${repo}`;
+};
+
+module.exports = {
+  keyword: 'docker',
+  action: 'openurl',
+  helper: {
+    title: 'Search for images in the Docker Registry',
+    subtitle: 'Example: docker node',
+    icon: {
+      path: './icon.png',
+    },
+  },
+  execute: q => new Promise(resolve => {
+    const opts = {
+      query: {
+        query: q,
+      },
+      json: true,
+    };
+    got(ENDPOINT, opts)
+      .then(res => {
+        if (res.body) {
+          const items = res.body.results
+            .map(i => Object.assign({}, {
+              title: i.repo_name,
+              subtitle: i.short_description,
+              arg: getRepositoryUrl(i),
+              icon: {
+                path: './icon.png',
+              },
+            }));
+          resolve({ items });
+        }
+      });
+  }),
+};

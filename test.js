b2620244599f984d82c181fd65c7cd5a3c57f4d8
Chris Helgert <chris.helgert@yahoo.com>
2016-10-02 21:20:45 +0200

    changed testrunner from ava to jest

--- test.js
+++
@@ -1,7 +0,0 @@
-import test from 'ava';
-import m from '.';
-
-test('retrieve some results from the api', async t => {
-  const results = await m.execute('node');
-  t.true(results.items.length > 0);
-});

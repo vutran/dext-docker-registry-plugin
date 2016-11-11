c62def79e00bbf8aa8a3292bf32db26657b08645
Vu Tran <vu@vu-tran.com>
2016-11-10 22:16:14 -0800

    mock http request

---
+++ __tests__/index.js
@@ -0,0 +1,24 @@
+const m = require('../index');
+
+jest.mock('got');
+
+describe('dext-docker-registry-plugin', () => {
+  it('retrieve some results from the api', async () => {
+    require('got').__setResponseBody({
+      results: [
+        {
+          star_count: 100,
+          pull_count: 100,
+          repo_owner: null,
+          short_description: 'Node.js is a JavaScript-based platform for server-side and networking applications.',
+          is_automated: false,
+          is_official: true,
+          repo_name: 'official/node',
+        },
+      ],
+    });
+
+    const results = await m.execute('node');
+    expect(results.items.length).toBeGreaterThan(0);
+  });
+});
--- __tests__/test.js
+++
@@ -1,6 +0,0 @@
-const m = require('../index');
-
-it('retrieve some results from the api', async () => {
-  const results = await m.execute('node');
-  expect(results.items.length > 0).toBeTruthy();
-});
